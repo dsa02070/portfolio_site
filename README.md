78e954c17b32003e8be48e5e9fbda3669256dbb2
dsa02070 <82920762+dsa02070@users.noreply.github.com>
2021-05-31 02:46:38 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# portpolio_site
+ 포트폴리오 담는 사이트
---
+++ js/main.js
@@ -0,0 +1,40 @@
+$(function () {
+
+    //top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() >= 100) {
+            $('.top_btn').addClass('on');
+        } else {
+            $('.top_btn').removeClass('on');
+        }
+    });
+
+    //mgnb click event
+    $('.mgnb_btn').click(function () {
+        $('#gnb').toggleClass('on');
+        // mobile btn span event 
+        $('.mgnb_btn a span:nth-child(1)').toggleClass('first_bar');
+        $('.mgnb_btn a span:nth-child(2)').toggleClass('opacity_zero');
+        $('.mgnb_btn a span:nth-child(3)').toggleClass('last_bar');
+    })
+
+    // top_btn, menu click event
+    $(".top_btn, #gnb ul li a").click(function () {
+        let thisElem = $(this.hash);
+        let offsetElem = thisElem.offset();
+        $("html,body").stop();
+        $("html,body").animate({ scrollTop: offsetElem.top }, 500);
+    });
+
+    // slick slider
+    $('.portpolio_content').slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        pauseOnHover: true,
+        customPaging: function (slider, i) {
+            console.log($(slider.$slides[i]).html());
+            return '<button class="tab">' + $(slider.$slides[i]).attr('title') + '</button>';
+        },
+    });
+})
